35fa604758f41735ff8c7f8c0eaf236ee34816d0
VladMiryaha <vlad.miryaha@gmail.com>
2016-10-02 02:04:36 +0300

    fix a couple of problems

--- task2/task_executor.c
+++ task2/task_executor.c
@@ -3,6 +3,7 @@
 #include "sys/wait.h"
 #include "stdlib.h"
 #include "string.h"
+
 #define MAX_TOKENS_COUNT 50
 
 void Split(char* string, char* delimiters,
@@ -11,7 +12,21 @@ void Split(char* string, char* delimiters,
 int main(int argc, char const *argv[])
 {
 	FILE *file;
-	char buffer[128];
+/*
+ * FIXIT:
+ * 128 - магическое число
+ */
+  char buffer[128];
+  
+  /*
+   * FIXIT:
+   * если хотите приложить к программе пример, то закоммитьте дополнительный файл test.txt
+   * + можно приложить файл readme.txt с комментарием использования...
+   * можно было бы написать код, будто считывается с консоли, а запускать так 
+   * ./a.out < test.txt
+   * 
+   * В код программы включать генерацию примера не нужно.
+   */
 	file = fopen("test.txt", "w");
 
 	fprintf(file, "3\n");
@@ -41,9 +56,18 @@ int main(int argc, char const *argv[])
 			Split(buffer, delimiters, &tokens, &tokensCount);
 			sleep(atoi(tokens[0]));
 			execvp(tokens[1], tokens + 1);
+      /*
+       * FIXIT:
+       * представьте, что требуется запустить задачу с несуществующим названием ...
+       * вы создаете новый процесс под это, а execvp возвращает какую-то ошибку ...
+       * дальше уже два процесса будут запускать одни и те же задачи ...
+       */
 		}
 	}
 
+	/*
+   * Можно было бы добавить код, чтобы запускающий процесс дождался завершения всех дочерних
+   */
 	for(i = 0; i < MAX_TOKENS_COUNT; i++)
 	{
 		free(tokens[i]);
@@ -51,17 +75,26 @@ int main(int argc, char const *argv[])
 	fclose(file);
 	return 0; 
 }
+
 void Split(char* string, char* delimiters,
 		   char*** tokens, int* tokensCount)
 {	
 	char** matrix = *tokens;
 	char* temp = NULL;
 	temp = strtok(string, delimiters);
+  
+  /*
+   * Можно не править, но просто представьте, что вам надо использовать чужую ф-ю, которая в случае ошибки печатает в консоль какой-то текст.
+   * Вы как ответственный программист хотите обрабатывать все пробленые случаи. У вас просто никак не получится это сделать.
+   * 
+   * Если хотите обрабатывать ошибки, то ф-я должна возвращать код ошибки, например.
+   */
 	if (temp == NULL)
 	{
 		printf("error\n");
 		return;
 	}
+	
 	while (temp != NULL)
 	{
 		strcpy((matrix)[*tokensCount], temp);
@@ -70,3 +103,7 @@ void Split(char* string, char* delimiters,
 	}
 	(matrix)[*tokensCount] = NULL;
 }
+
+/*
+ * В целом код хороший. Нужно только поправить небольшие замечания.
+ */
